262e4dd13bcd5686e231defc647fecc43bc9dc2d
Juan Ignacio Vaccarezza <jivaccarezza@invap.com.ar>
2024-04-26 09:54:50 -0300

    Add copyright notice to files

--- src/doorstop/document_tree.rs
+++ src/doorstop/document_tree.rs
@@ -1,3 +1,21 @@
+/// doorstop-rs: Help library to read doorstop documents implemented in Rust.
+/// Copyright (C) <2024>  INVAP S.E.
+///
+/// This file is part of doorstop-rs.
+///
+/// doorstop-rs is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU Affero General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU Affero General Public License for more details.
+///
+/// You should have received a copy of the GNU Affero General Public License
+/// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 use crate::doorstop::document::Document;
 use crate::doorstop::file;
 use walkdir::WalkDir;
